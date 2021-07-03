fb4fd854c37213a59b166792f2132b968a26a091
ekanshg <ekanshg@zeta.tech>
2021-07-04 00:45:29 +0530

    profileID

--- Main/speechrecog.py
+++ Main/speechrecog.py
@@ -1,18 +1,37 @@
 import requests
 import io
+from flask import Flask,request,jsonify,render_template
+from flask_cors import CORS, cross_origin
+import os
+from subprocess import call, Popen, PIPE
+import json
+
+
+app = Flask(__name__)
+
+@app.route('/home',methods=['GET','POST'])
+@cross_origin(supports_credentials=True)
+def dosomething():
+    response = sendRequest()
+    json_data = json.loads(response.text)
+    print(json_data['profileId'])
+    return "abcd"
+
 def sendRequest():
 
-    requestUrl = "https://westus.api.cognitive.microsoft.com/speaker/verification/v2.0/text-dependent/profiles"
+    requestUrl = "https://westus.api.cognitive.microsoft.com/speaker/verification/v2.0/text-dependent/profiles/"
 
     headers = {
     "Content-Type" : "application/json",
     "Ocp-Apim-Subscription-Key" : "fe29f57488b04a81a330347d50b1a599",
     }
 
-    locale="en-us"
+    params = {
+        'locale':'en-us'
+    }
     # this was a file which was on my local machine so change it accordingly
 
-    response = requests.request("POST",requestUrl,data=locale,headers = headers)
+    response = requests.request("POST",requestUrl,json=params,headers = headers)
     return response
 
 def getResponse(path,profileID):
@@ -42,3 +61,7 @@ def identify(path,profileID):
     body = open(path ,"rb").read()
     response = requests.request("POST",requestUrl,headers = headers,data=body)
     return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

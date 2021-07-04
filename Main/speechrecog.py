ed1571d8cbcf9dbafee5431e8b609c070095e3cf
ekanshg <ekanshg@zeta.tech>
2021-07-04 15:44:10 +0530

    getResponse fixed

--- Main/speechrecog.py
+++ Main/speechrecog.py
@@ -5,11 +5,11 @@ from flask_cors import CORS, cross_origin
 import os
 from subprocess import call, Popen, PIPE
 import json
-
+import wave
 
 app = Flask(__name__)
 #profileID="4d1cfca2-4249-41f7-8102-9f11fd692d48"
-path="C:\\Users\\nandi\\OneDrive\\Documents\\GitHub\\FaceTag-corona\\Main\\uploads\\record.wav"
+path="uploads/record.wav"
 
 @app.route('/home',methods=['GET','POST'])
 @cross_origin(supports_credentials=True)
@@ -49,17 +49,19 @@ def sendRequest():
     return response
 
 def getResponse(path,profileID):
-    requestUrl = "https://westus.api.cognitive.microsoft.com/speaker/verification/v2.0/text-dependent/profiles/{profileID}/enrollments"
+    requestUrl = "https://westus.api.cognitive.microsoft.com/speaker/verification/v2.0/text-dependent/profiles/{0}/enrollments"
 
     headers = {
     "Content-Type" : "audio/wav",
     "Ocp-Apim-Subscription-Key" : "fe29f57488b04a81a330347d50b1a599",
     }
-    body = open(path ,"rb").read()
+    w = open(path, 'rb')
+    # binary_data = w.readframes(w.getnframes())
+
     print(profileID)
     # this was a file which was on my local machine so change it accordingly
 
-    response = requests.request("POST",requestUrl,data=path,headers = headers)
+    response = requests.request("POST",requestUrl.format(profileID),data=w,headers = headers)
     return response
 
 def identify(path,profileID):
